d93eacd2c2811390a2c2fe41cf2e1388350dac0d
Mathias Breistein <mathias.breistein@gmail.com>
2025-01-01 22:32:46 +0100

    Last attempt!

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,6 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
@@ -36,8 +31,20 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#4A86C2" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={inter.className}>
         {children}
